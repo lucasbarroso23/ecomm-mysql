7205cfdfabd804ce9cc1f644c3b9856c3189e500
lucasbarroso23 <lucasbarroso23@gmail.com>
2024-10-24 01:25:41 +0100

    update and delete product

--- ecomm-api/storer/storer_mysql.go
+++ ecomm-api/storer/storer_mysql.go
@@ -51,3 +51,21 @@ func (ms *MySQLStorer) ListProducts(ctx context.Context) ([]*Product, error) {
 
 	return products, nil
 }
+
+func (ms *MySQLStorer) UpdateProduct(ctx context.Context, p *Product) (*Product, error) {
+	_, err := ms.db.NamedExecContext(ctx, "UPDATE products SET name=:name, image=:image, category=:category, description=:description, rating=:rating, num_reviews=:num_reviews, price=:price, count_in_stock=:count_in_stock WHERE id=:id", p)
+	if err != nil {
+		return nil, fmt.Errorf("error updating product: %w", err)
+	}
+
+	return p, nil
+}
+
+func (ms *MySQLStorer) DeleteProduct(ctx context.Context, id int64) error {
+	_, err := ms.db.ExecContext(ctx, "DELETE FROM products WHERE id=:?", id)
+	if err != nil {
+		return fmt.Errorf("error deleting product: %w", err)
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/lucasbarroso23/ecomm-mysql
 go 1.22.0
 
 require github.com/jmoiron/sqlx v1.4.0
+
+require github.com/DATA-DOG/go-sqlmock v1.5.2 // indirect
